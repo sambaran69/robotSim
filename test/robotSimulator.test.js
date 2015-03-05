2c57b122756159bd75115e8de91b69dd13155bd0
Sambaran Roy <sambaran.roy@emids.com>
2015-03-05 10:09:52 +0530

    Added logic for Robot Simulator and modified test cases

--- test/robotSimulator.test.js
+++ test/robotSimulator.test.js
@@ -1,85 +1,86 @@
 var RobotSimulator = require('../lib/robotSimulator');
 var Robot = require('../lib/robot');
-var assert = require('assert');
+var assert = require('chai').assert;
 
-describe('Robot Simulator Tests', function() {
-    it('Robot Simulator Can Be Initialised Properly', function(done) {
+describe('Robot Simulator', function() {
+    it('Robot Simulator Can Be Initialised Properly', function() {
         var simulator = new RobotSimulator(new Robot());
-        assert.equal(success, false);
+        assert.isObject(simulator);
+        assert.instanceOf(simulator,RobotSimulator);
     });
 
-    it('Empty Command Is Reported Invalid', function(done) {
+    it('Empty Command Is Reported Invalid', function() {
         var simulator = new RobotSimulator(new Robot());
         var response = simulator.Command('');
         assert.equal(response, false);
     });
 
-    it('Unrecognized Command Is Reported Invalid', function(done) {
+    it('Unrecognized Command Is Reported Invalid', function() {
         var simulator = new RobotSimulator(new Robot());
-        var response = simulator.Command('jump');
+        var response = simulator.Command('JUMP');
         assert.equal(response, false);
     });
 
-    it('Recognized Command But Is Reported Invalid', function(done) {
+    it('Recognized Command But Is Reported Invalid', function() {
         var simulator = new RobotSimulator(new Robot());
-        var response = simulator.Command('move');
+        var response = simulator.Command('MOVE');
         assert.equal(response, false);
     });
 
-    it('PLACE Command With No Arguments Reports Invalid', function(done) {
+    it('PLACE Command With No Arguments Reports Invalid', function() {
         var simulator = new RobotSimulator(new Robot());
-        var response = simulator.Command('place');
+        var response = simulator.Command('PLACE');
         assert.equal(response, false);
     });    
 
-    it('PLACE Command With Invalid Arguments Reports Invalid', function(done) {
+    it('PLACE Command With Invalid Arguments Reports Invalid', function() {
         var simulator = new RobotSimulator(new Robot());
-        var response = simulator.Command('place abcd');
+        var response = simulator.Command('PLACE abcd');
         assert.equal(response, false);
-        var response = simulator.Command('place 1,a,north');
+        var response = simulator.Command('PLACE 1,a,NORTH');
         assert.equal(response, false);
-        var response = simulator.Command('place b,1,north');
+        var response = simulator.Command('PLACE b,1,NORTH');
         assert.equal(response, false);
-        var response = simulator.Command('place 1,1,southeast');
+        var response = simulator.Command('PLACE 1,1,SOUTHEAST');
         assert.equal(response, false);        
     });
 
-    it('Placed And Turned Left Reports Correct Position', function(done) {
+    it('Robot Placed And Turned Left Reports Correct Position', function() {
         var simulator = new RobotSimulator(new Robot());
-        simulator.Command('place 1,1,north');
-        simulator.Command('left');
-        assert.equal(simulator.Command('report'), '1,1,west');
+        simulator.Command('PLACE 1,1,NORTH');
+        simulator.Command('LEFT');
+        assert.equal(simulator.Command('REPORT'), '1, 1, WEST');
     });
 
-    it('Placed And Turned Right Reports Correct Position', function(done) {
+    it('Robot Placed And Turned Right Reports Correct Position', function() {
         var simulator = new RobotSimulator(new Robot());
-        simulator.Command('place 1,1,north');
-        simulator.Command('right');
-        assert.equal(simulator.Command('report'), '1,1,east');
+        simulator.Command('PLACE 1,1,NORTH');
+        simulator.Command('RIGHT');
+        assert.equal(simulator.Command('REPORT'), '1, 1, EAST');
     }); 
 
-    it('Placed But Cannot be Moved Off Table', function(done) {
+    it('Robot Placed But Cannot be Moved Off Table', function() {
         var simulator = new RobotSimulator(new Robot());
-        simulator.Command('place 5,5,north');
-        var response = simulator.Command('move');
+        simulator.Command('PLACE 4,4,NORTH');
+        var response = simulator.Command('MOVE');
         assert.equal(response, false);
-        assert.equal(simulator.Command('report'), '5,5,north');
+        assert.equal(simulator.Command('REPORT'), '4, 4, NORTH');
     });
 
-    it('Placed And Moved Reports Correct Position', function(done) {
+    it('Robot Placed And Moved Reports Correct Position', function() {
         var simulator = new RobotSimulator(new Robot());
-        simulator.Command('place 1,1,north');
-        simulator.Command('move');
-        assert.equal(simulator.Command('report'), '1,2,north');
+        simulator.Command('PLACE 1,1,NORTH');
+        simulator.Command('MOVE');
+        assert.equal(simulator.Command('REPORT'), '1, 2, NORTH');
     });
 
-    it('Placed, Moved And Turned Reports Correct Position', function(done) {
+    it('Robot Placed, Moved And Turned Reports Correct Position', function() {
         var simulator = new RobotSimulator(new Robot());
-        simulator.Command('place 1,1,east');
-        simulator.Command('move');
-        simulator.Command('move');
-        simulator.Command('left');
-        simulator.Command('move');
-        assert.equal(simulator.Command('report'), '3,3,north');
+        simulator.Command('PLACE 1,1,EAST');
+        simulator.Command('MOVE');
+        simulator.Command('MOVE');
+        simulator.Command('LEFT');
+        simulator.Command('MOVE');
+        assert.equal(simulator.Command('REPORT'), '3, 2, NORTH');
     });
 });
