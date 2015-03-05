dd2c9bcc3dc88e27b2294d123deab33bd7e0eb4d
Sambaran Roy <sambaran.roy@emids.com>
2015-03-05 07:07:03 +0530

    Implemented logic for Robot and fixed tests

--- test/robot.test.js
+++ test/robot.test.js
@@ -1,23 +1,30 @@
 var Robot = require('../lib/robot');
-var assert = require('assert');
+var assert = require("chai").assert;
 
-describe('Robot Tests', function() {
-    it('Initialised But Not Placed Robot Cannot Be Moved', function(done) {
+var Facing = {
+    North: 'NORTH',
+    South: 'SOUTH',
+    East: 'EAST',
+    West: 'WEST'
+};
+
+describe('Robot', function() {
+    it('Initialised But Not Placed Robot Cannot Be Moved', function() {
         var robot = new Robot();
         var success = robot.Move();
         assert.equal(success, false);
     });
 
-    it('Initialised But Not Placed Robot Cannot Be Turned', function(done) {
+    it('Initialised But Not Placed Robot Cannot Be Turned', function() {
         var robot = new Robot();
         var success = robot.Left();
         assert.equal(success, false);
     });
 
-    it('Initialised But Not Placed Robot Cannot Report Its Position', function(done) {
+    it('Initialised But Not Placed Robot Cannot Report Its Position', function() {
         var robot = new Robot();
         var position = robot.Report();
-        assert.equal(position,'');
+        assert.equal(position,'Robot needs to be placed on the table');
     });
 
     it('Robot Cannot Be Placed Off Table', function() {
@@ -26,50 +33,50 @@ describe('Robot Tests', function() {
         assert.equal(success, false);
     });
 
-    it('Robot When Placed Can Report Its Position', function(done) {
+    it('Robot When Placed Can Report Its Position', function() {
         var robot = new Robot();
         var success = robot.Place(3, 2, Facing.East);
         var position = robot.Report();
         assert.equal(success, true);
-        assert.equal(position, '3,2,EAST');
+        assert.equal(position, '3, 2, EAST');
     });
 
-    it('Robot Placed And Turns Left Reports Correct Position', function(done) {
+    it('Robot Placed And Turns Left Reports Correct Position', function() {
         var robot = new Robot();
         robot.Place(1, 1, Facing.North);
         robot.Left();
-        assert.equal(robot.Report(),'1,1,WEST');
+        assert.equal(robot.Report(),'1, 1, WEST');
     });
 
-    it('Robot Placed And Turns Right Reports Correct Position', function(done) {
+    it('Robot Placed And Turns Right Reports Correct Position', function() {
         var robot = new Robot();
         robot.Place(1, 1, Facing.North);
         robot.Right();
-        assert.equal(robot.Report(),'1,1,EAST');
+        assert.equal(robot.Report(),'1, 1, EAST');
     });
 
-    it('Robot Placed But Cannot be Moved Off Table', function(done) {
+    it('Robot Placed But Cannot be Moved Off Table', function() {
         var robot = new Robot();
-        robot.Place(5, 5, Facing.North);
+        robot.Place(4, 4, Facing.North);
         var success = robot.Move();
         assert.equal(success, false);
-        assert.equal(robot.Report(), '5,5,NORTH');
+        assert.equal(robot.Report(), '4, 4, NORTH');
     });
 
-    it('Robot Placed And Moved Reports Correct Position', function(done) {
+    it('Robot Placed And Moved Reports Correct Position', function() {
         var robot = new Robot();
         robot.Place(1, 1, Facing.North);
         robot.Move();
-        assert.equal(robot.Report(), '1,2,NORTH');
+        assert.equal(robot.Report(), '1, 2, NORTH');
     });
 
-    it('Robot Placed, Moved And Turned Reports Correct Position', function(done) {
+    it('Robot Placed, Moved And Turned Reports Correct Position', function() {
         var robot = new Robot();
         robot.Place(1, 2, Facing.East);
         robot.Move();
         robot.Move();
         robot.Left();
         robot.Move();
-        assert.equal(robot.Report(), '3,3,NORTH');
+        assert.equal(robot.Report(), '3, 3, NORTH');
     });
 });
