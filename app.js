9dba24d528bb008c1d4b393cb1f23b1ffd599d55
Sambaran Roy <sambaran.roy@emids.com>
2015-03-06 03:49:43 +0530

    Added Sample Commands file that can be passed as agrument to the simulator

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var RobotSimulator = require('./lib/robotSimulator');
 var Robot = require('./lib/robot');
+var fs = require('fs');
 
-var simulator = new RobotSimulator(new Robot(), true);
+var options = {};
+var args = process.argv.slice(2);
+
+if (args && args[0] === '-file') {
+	if (args[1] && fs.existsSync(args[1]))
+		options = { input: fs.createReadStream(args[1]) }			
+	else
+		console.log('Unknown File: Not Able To Find The File ', args[1]);
+}
+
+var simulator = new RobotSimulator(new Robot(), true, options);
